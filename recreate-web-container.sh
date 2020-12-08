8bbdd569416f824fe9442bd81884929aa9e177db
keshav nayyar <38125723+Flaircrafter@users.noreply.github.com>
2020-12-09 00:29:43 +0530

    Update recreate-web-container.sh

--- recreate-web-container.sh
+++ recreate-web-container.sh
@@ -5,13 +5,9 @@ do
     if [[ $(docker port "${id}") == *"${1}"* ]]; then
         echo "stopping container ${id}"
         docker stop "${id}"
-        echo "collecting running container's image id"
-        cont-image=$(docker container inspect "${id}" --format '{{.Image}}')
-        echo "Image id is - $cont-image"
         echo "Removing container"
         docker rm -vf "${id}"
-        echo "Removing image id $cont-image"
-        docker rmi -f $cont-image
+        docker rmi -f maven_project:latest
         echo "Finish"
     fi
 done
